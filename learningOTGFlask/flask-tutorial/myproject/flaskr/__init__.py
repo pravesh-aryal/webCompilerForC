35fb504f15f37bcb49736338e896974fcc7f955b
pravesh <pravesh.aryal696969@gmail.com>
2024-11-24 12:58:27 -0600

    make the project installable

--- learningOTGFlask/flask-tutorial/myproject/flaskr/__init__.py
+++ learningOTGFlask/flask-tutorial/myproject/flaskr/__init__.py
@@ -30,54 +30,12 @@ def create_app(test_config = None):
     def hello():
         return "Hello world"
 
-    @app.route('/home')
-    def home():
-        return "THIS IS THE HOME PAGE"
-    @app.route('/<name>')
-    def suyog(name):
-
-        # return "<h1 style='background-color:DodgerBlue'> Suyong randi ko ban ho muji bhode khate sala </h1>"
-        return """<marquee>
-            <h1 style="background-color:DodgerBlue; text-align:center"> {name=name} </h1>
-            </marquee>
-        """
-        return """
- <html>
-<head>
-    <title>SUYOG Fullscreen</title>
-    <style>
-        body {
-            margin: 0;
-            overflow: hidden;
-            background-color: black;
-        }
-
-        h1 {
-            font-size: 100px;
-            font-weight: bold;
-            text-align: center;
-            color: white;
-            text-shadow: 2px 2px 4px black;
-            animation: colorChange 5s infinite alternate;
-        }
-
-        @keyframes colorChange {
-            0% { color: red; }
-            25% { color: green; }
-            50% { color: blue; }
-            75% { color: yellow; }
-            100% { color: purple; }
-        }
-    </style>
-</head>
-<body>
-    <h1>SUYOG</h1>
-</body>
-</html>
-"""
-
     from . import auth
     app.register_blueprint(auth.bp)
-    return app
 
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
 
